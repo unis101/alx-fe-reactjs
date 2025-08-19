fbda8c8caf98e30d28fa3adf26d54b44cded5813
Eunice <eunicewambui22@gmail.com>
2025-08-20 00:04:55 +0300

    Add header, main content, footer components

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -8,11 +8,6 @@ function App() {
     <div>
       <Header />
       <MainContent />
-      <UserProfile 
-        name="Alice" 
-        age="25" 
-        bio="Loves hiking and photography" 
-      />
       <Footer />
     </div>
   );
